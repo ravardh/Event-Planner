03e46c08808420e1a6a9d3b36eda55129d42bf4d
Raj Vardhan <errvmaurya563@gmail.com>
2025-06-20 20:29:19 +0530

    Hero, Login, Register, Pages done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Hero from "./components/Hero";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <>
-      <div className='text-red-500'>App</div>
+      <BrowserRouter>
+        <Navbar />
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
